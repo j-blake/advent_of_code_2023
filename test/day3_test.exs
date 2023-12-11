cd549761fd94b4b7961606991980cc8c8b93fb11
Blake McAskill <10172427+j-blake@users.noreply.github.com>
2023-12-11 07:44:23 -0500

    day 3 part 2: calculate sum of gear ratios

--- test/day3_test.exs
+++ test/day3_test.exs
@@ -1,11 +1,19 @@
 defmodule Day3Test do
   use ExUnit.Case
 
-  test "it calculates example input" do
+  test "it calculates example input for Part 1" do
     assert Day3.part_1() === 4361
   end
 
-  test "it calculates puzzle input" do
+  test "it calculates puzzle input for Part 1" do
     assert Day3.part_1("day3/input.txt") === 520135
+  end
+
+  test "it calculates example input for Par 2" do
+    assert Day3.part_2() === 467835
+  end
+
+  test "it calculates puzzle input for Part 2" do
+    assert Day3.part_2("day3/input.txt") === 72514855
   end
 end
