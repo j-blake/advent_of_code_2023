9df1fcab6d94f9bde0d26e9c6a56769198a97c93
Blake McAskill <10172427+j-blake@users.noreply.github.com>
2023-12-12 20:52:16 -0500

    day 4 part 2: find total scratchcards won

--- test/day4_test.exs
+++ test/day4_test.exs
@@ -1,11 +1,21 @@
 defmodule Day4Test do
   use ExUnit.Case
 
+  doctest Day4
+
   test "it calculates example input for Part 1" do
     assert Day4.part_1() === 13
   end
 
   test "it calculates puzzle input for Part 1" do
     assert Day4.part_1("day4/input.txt") === 21213
+  end
+
+  test "it calculates example input for Part 2" do
+    assert Day4.part_2() === 30
+  end
+
+  test "it calculates puzzle input for Part 2" do
+    assert Day4.part_2("day4/input.txt") === 8549735
   end
 end
